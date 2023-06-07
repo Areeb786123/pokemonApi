6f0466aaecf2f0c962b119dab7c9ea8839cf5481
areeb786 <areebrockzz786@gmail.com>
2023-06-07 16:05:04 +0530

    added post request

--- src/main/java/com/pokemon/pokemon/controllers/PokemonController.java
+++ src/main/java/com/pokemon/pokemon/controllers/PokemonController.java
@@ -3,9 +3,7 @@ package com.pokemon.pokemon.controllers;
 import com.pokemon.pokemon.models.PokemonModels;
 import com.pokemon.pokemon.repository.PokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class PokemonController {
     @GetMapping("/getAllPokemons")
     public List<PokemonModels> getAllPokemons() {
         return pokemonRepository.findAll();
+    }
+
+    @PostMapping("/addPokemon")
+    public PokemonModels addPokemon(@RequestBody  PokemonModels pokemonData) {
+        return pokemonRepository.save(pokemonData);
     }
 }
